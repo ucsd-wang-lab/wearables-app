abeaccf9d83718d39e02ef196d8352c2bd8babdf
Ravi Patel <rdp2606@gmail.com>
2020-04-16 10:39:55 -0700

    added refresh functionality to ble scanner

--- Bluetooth_connect/Bluetooth/BTSelectionScreen.swift
+++ Bluetooth_connect/Bluetooth/BTSelectionScreen.swift
@@ -9,42 +9,13 @@
 import UIKit
 import CoreBluetooth
 
-class BTSelectionScreen: UIViewController, UITableViewDataSource, UITableViewDelegate, BLEDiscoveredObserver, BLEStatusObserver, UIScrollViewDelegate{
+class BTSelectionScreen: UIViewController, UITableViewDataSource, UITableViewDelegate{
     var id: Int = 0
-        
-    func update<T>(with name: String, with device: T){
-        bleDeviceList.updateValue(device as! CBPeripheral, forKey: name)
-//        UIView.transition(with: bluetoothTableView, duration: 1.0, options: .transitionCrossDissolve, animations: {self.bluetoothTableView.reloadData()}, completion: nil)
-
-        bluetoothTableView.reloadData()
-    }
-    
-    func deviceConnected(with device: String) {
-        let storyboard = UIStoryboard(name: "Dashboard", bundle: nil)
-        let controller = storyboard.instantiateInitialViewController() as! DashboardViewController
-        controller.modalPresentationStyle = .fullScreen
-        controller.deviceName = device
-        self.present(controller, animated: true) {
-            self.bleDeviceList.removeAll()
-            BluetoothInterface.instance.detachBLEDiscoveredObserver(id: self.id)
-            BluetoothInterface.instance.detachBLEStatusObserver(id: self.id)
-            BluetoothInterface.instance.stopScan()
-        }
-    }
-    
-    func didBTEnable(with value: Bool) {
-        if value == false{
-            let alert = UIAlertController(title: "Error: Bluetooth Off!!", message: "Please turn on Bluetooth on phone!", preferredStyle: .alert)
-
-            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
-            self.present(alert, animated: true)
-        }
-    }
-
 
     @IBOutlet weak var bluetoothTableView: UITableView!
     
     var bleDeviceList: [String: CBPeripheral] = [:]
+    let refreshControl = UIRefreshControl()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -55,6 +26,8 @@ class BTSelectionScreen: UIViewController, UITableViewDataSource, UITableViewDel
         BluetoothInterface.instance.attachBLEDiscoveredObserver(id: id, observer: self)
         BluetoothInterface.instance.attachBLEStatusObserver(id: id, observer: self)
         
+        bluetoothTableView.addSubview(refreshControl)
+        refreshControl.addTarget(self, action: #selector(refreshBluetoothTableView), for: .valueChanged)
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -91,4 +64,42 @@ class BTSelectionScreen: UIViewController, UITableViewDataSource, UITableViewDel
         BluetoothInterface.instance.connect(peripheral: bleDeviceList[indexPath.row].value)
     }
     
+    @objc func refreshBluetoothTableView(_ sender: Any){
+        bleDeviceList.removeAll()
+        bluetoothTableView.reloadData()
+        refreshControl.endRefreshing()
+        BluetoothInterface.instance.startScan()
+    }
+    
+}
+
+extension BTSelectionScreen: BLEDiscoveredObserver, BLEStatusObserver{
+    func update<T>(with name: String, with device: T){
+        bleDeviceList.updateValue(device as! CBPeripheral, forKey: name)
+//        UIView.transition(with: bluetoothTableView, duration: 1.0, options: .transitionCrossDissolve, animations: {self.bluetoothTableView.reloadData()}, completion: nil)
+
+        bluetoothTableView.reloadData()
+    }
+        
+    func deviceConnected(with device: String) {
+        let storyboard = UIStoryboard(name: "Dashboard", bundle: nil)
+        let controller = storyboard.instantiateInitialViewController() as! DashboardViewController
+        controller.modalPresentationStyle = .fullScreen
+        controller.deviceName = device
+        self.present(controller, animated: true) {
+            self.bleDeviceList.removeAll()
+            BluetoothInterface.instance.detachBLEDiscoveredObserver(id: self.id)
+            BluetoothInterface.instance.detachBLEStatusObserver(id: self.id)
+            BluetoothInterface.instance.stopScan()
+        }
+    }
+        
+    func didBTEnable(with value: Bool) {
+        if value == false{
+            let alert = UIAlertController(title: "Error: Bluetooth Off!!", message: "Please turn on Bluetooth on phone!", preferredStyle: .alert)
+
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            self.present(alert, animated: true)
+        }
+    }
 }
