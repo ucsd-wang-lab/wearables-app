3029a78e7085b38914d4e5ccc2b640fd6388f2cd
Ravi Patel <rdp2606@gmail.com>
2020-07-07 18:29:27 -0700

    some auto-layout fixed

--- Bluetooth_connect/TestConfigurationViewController.swift
+++ Bluetooth_connect/TestConfigurationViewController.swift
@@ -10,20 +10,16 @@ import UIKit
 
 class TestConfigurationViewController: UIViewController {
 
+    @IBOutlet weak var nextButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        nextButton.layer.cornerRadius = nextButton.layer.bounds.height / 3
+
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func nextButtonClicked(_ sender: Any) {
+        performSegue(withIdentifier: "toTestName", sender: self)
     }
-    */
 
 }
