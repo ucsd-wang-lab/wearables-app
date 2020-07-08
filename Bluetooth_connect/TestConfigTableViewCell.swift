0b4b5685d1642398b65d8980435a581ba486d330
Ravi Patel <rdp2606@gmail.com>
2020-07-07 19:48:08 -0700

    Test Configuration UI setup done

---
+++ Bluetooth_connect/TestConfigTableViewCell.swift
@@ -0,0 +1,66 @@
+//
+//  TestConfigTableViewCell.swift
+//  Bluetooth_connect
+//
+//  Created by Ravi Patel on 7/7/20.
+//  Copyright © 2020 neel shah. All rights reserved.
+//
+
+import UIKit
+
+class TestConfigTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var displayView: UIView!
+    var keyLabel: UILabel!
+    var valueLabel: UITextField!
+    var unitsLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        keyLabel = UILabel()
+        valueLabel = UITextField()
+        unitsLabel = UILabel()
+
+        keyLabel.textColor = UIColor(red: 0x31/255, green: 0x30/255, blue: 0x30/255, alpha: 1)
+        keyLabel.font = UIFont(name: "Avenir-Heavy", size: 20)
+        
+        valueLabel.textColor = UIColor(red: 0x31/255, green: 0x30/255, blue: 0x30/255, alpha: 1)
+        valueLabel.font = UIFont(name: "Avenir-Regular", size: 16)
+        valueLabel.textAlignment = .right
+        valueLabel.keyboardType = .numbersAndPunctuation
+        valueLabel.clearButtonMode = .whileEditing
+        
+        unitsLabel.textColor = UIColor(red: 0x31/255, green: 0x30/255, blue: 0x30/255, alpha: 1)
+        unitsLabel.font = UIFont(name: "Avenir-Regular", size: 16)
+        unitsLabel.textAlignment = .left
+        unitsLabel.adjustsFontSizeToFitWidth = true
+        
+//        keyLabel.backgroundColor = .red
+//        valueLabel.backgroundColor = .orange
+//        unitsLabel.backgroundColor = .green
+                
+        let width = displayView.frame.width
+        let height = displayView.frame.height
+        
+        let key_label_fram = CGRect(x: 0, y: 0, width: width * 0.5, height: height)
+        keyLabel.frame = key_label_fram
+
+        let value_label_frame = CGRect(x: keyLabel.frame.maxX, y: 0, width: width * 0.3, height: height)
+        valueLabel.frame = value_label_frame
+        
+        let suffix_label_frame = CGRect(x: valueLabel.frame.maxX, y: 0, width: width * 0.2, height: height)
+        unitsLabel.frame = suffix_label_frame
+        
+        displayView.addSubview(keyLabel)
+        displayView.addSubview(valueLabel)
+        displayView.addSubview(unitsLabel)
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- Bluetooth_connect/TestConfigurationViewController.swift
+++ Bluetooth_connect/TestConfigurationViewController.swift
@@ -8,14 +8,57 @@
 
 import UIKit
 
-class TestConfigurationViewController: UIViewController {
-
+class TestConfigurationViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var testConfigTableView: UITableView!
     @IBOutlet weak var nextButton: UIButton!
+    
+    var unitsMapping: [Int: [String: String]] = [0: ["Bias Potential": " mV"],
+                                                 1: ["Initial Delay": " ms"],
+                                                 2: ["Sample Period": " ms"],
+                                                 3: ["Sample Count": ""],
+                                                 4: ["Gain": " k\u{2126}"],
+                                                 5: ["Electrode Mask": ""]
+                                                ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         nextButton.layer.cornerRadius = nextButton.layer.bounds.height / 3
 
+        testConfigTableView.delegate = self
+        testConfigTableView.dataSource = self
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return unitsMapping.count
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        1
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 70
+    }
+    
+    func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        return 1
+    }
+    func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
+        let view = UIView(frame: CGRect(x: 0, y: 0, width: tableView.bounds.width, height: 1.0))
+        view.backgroundColor = UIColor(red: 0x41/255, green: 0xb2/255, blue: 0x5b/255, alpha: 1)
+        
+        return view
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "testConfigCell") as! TestConfigTableViewCell
+        cell.keyLabel.text = "\(unitsMapping[indexPath.section]![0].key)"
+        cell.valueLabel.text = "xxxx xxxx"
+        cell.unitsLabel.text = " \(unitsMapping[indexPath.section]![0].value)"
+//        print("Section = \(indexPath.section)\t \(unitsMapping[indexPath.section]![0].key)")
+        return cell
     }
     
     @IBAction func nextButtonClicked(_ sender: Any) {
